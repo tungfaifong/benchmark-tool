f8b19c54ccf577033e9446f2b8669f6869fa5cf8
tungfaifong <iam@tungfaifong.com>
2022-01-28 17:51:09 +0800

    自定义协议

--- module/module.cpp
+++ module/module.cpp
@@ -12,35 +12,32 @@ char *ptr = NULL;
 
 int encodeRequest(char* data, int maxLen)
 {
+    const char * ping = "ping";
+    auto size = strlen(ping);
+
     //只做一次初始化
     if (ptr == NULL)
     {
-        sprintf(buf, "%s%s%s%s%s%s%s%s%s%s",
-                "GET /lp?id=641023139&type=12&callback=callbackarea&rot=1 HTTP/1.1\r\n",
-                "Host: xx.com\r\n",
-                "Connection: keep-alive\r\n",
-                "User-Agent: Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11\r\n",
-                "Referer: http://xx.com/\r\n",
-                "Accept: */*\r\n",
-                "Accept-Encoding: gzip,deflate,sdch\r\n",
-                "Accept-Language: zh-CN,zh;q=0.8,en;q=0.6\r\n",
-                "Accept-Charset: gb18030,utf-8;q=0.7,*;q=0.3\r\n",
-                "\r\n");
+
+        memset(buf, 0, 4096);
+        memcpy(buf, &size, sizeof(uint16_t));
+        memcpy(buf + sizeof(uint16_t), ping, size);
+
         ptr = buf;
     }
 
-    int len = strlen(ptr);
+    int len = sizeof(uint16_t) + size;
        
     if (len > maxLen) 
         return -1;
 
     memcpy(data, ptr, len);
 
-	return len;
+    return len;
 }
 
 int decodeResponse(char* data, int len)
 {
-	return 0;
+    return 0;
 }
 
